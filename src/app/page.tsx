8a93e121bfc93eb84b241f0ea8ea84a1131cb0e5
Thiago <thiago.fontanella@hotmail.com>
2026-03-05 23:35:06 -0300

    feat: create suspense component and understand nextjs loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
-import { SpinLoader } from "@/components/SpinLoader/SpinLoader"
+import PostsList from "@/components/PostsList/PostsList";
+import { SpinLoader } from "@/components/SpinLoader/SpinLoader";
+import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
     <div>
-      <SpinLoader containerClasses="min-h-40"/>
+      <header>
+        <h1 className="text-6xl font-bold text-center py-8">Header</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer className="text-4xl font-bold text-center py-8">
+        <h2>Footer</h2>
+      </footer>
+
     </div>
   )
 }
---
+++ src/components/PostsList/PostsList.tsx
@@ -0,0 +1,13 @@
+import { postRepository } from "@/repositories/post"
+
+export default async function PostsList() {
+    const posts = await postRepository.findAll();
+
+    return (
+        <>
+            {posts.map((post) => {
+                return <p key={post.id}>{post.title}</p>
+            })}
+        </>
+    )
+}
