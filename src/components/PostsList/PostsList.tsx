fc71c2e8aa4c0f1525fe08d797261d0bd9d2001d
Thiago <thiago.fontanella@hotmail.com>
2026-03-12 23:15:55 -0300

    chore: start modelling posts list component

--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,12 +1,24 @@
 import { postRepository } from "@/repositories/post"
+import Image from "next/image";
 
 export default async function PostsList() {
     const posts = await postRepository.findAll();
 
     return (
-        <div className="pt-8">
+        <div className="pt-8 grid grid-cols-3 gap-8">
             {posts.map((post) => {
-                return <p key={post.id}>{post.title}</p>
+                return (
+                    <div key={`post_${post.id}`}>
+                        <Image src={post.coverImageUrl} alt={post.content} width={1200} height={720}/>
+                        <div className="flex flex-col">
+                            <p>{post.author}</p>
+
+                            <p>{post.title}</p>
+
+                            <p>{post.excerpt}</p>
+                        </div>
+                    </div>
+                )
             })}
         </div>
     )
