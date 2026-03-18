f9c5ab3572b4c06bad210e74ea9bdd5d9f9952fc
Thiago <thiago.fontanella@hotmail.com>
2026-03-17 23:05:20 -0300

    feat: create about page to understand nextjs metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Container } from "@/components/Container/Container";
+import { Header } from "@/components/Header/Header";
 
 export const metadata: Metadata = {
-  title: "The Blog",
+  title: {
+    default: 'The Blog - Next.js',
+    template: '%s | The Blog'
+  },
   description: "Blog com Next.js",
 };
 
@@ -14,7 +19,13 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="pt-BR">
       <body>
+        <Container>
+          <Header />
+        
           {children}
+
+          Footer
+        </Container>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,7 @@ import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <FeaturedPost />
       </Suspense>
@@ -17,10 +15,6 @@ export default async function HomePage() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer className="text-4xl font-bold text-center py-8">
-        <h2>Footer</h2>
-      </footer>
-    </Container>
+    </>
   )
 }
