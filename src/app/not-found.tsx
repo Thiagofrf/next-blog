a4d810f657ab58c0a6286172cf7269a6f532579c
Thiago <thiago.fontanella@hotmail.com>
2026-03-17 23:26:57 -0300

    feat: create custom not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+    return (
+        <div className="flex justify-center items-center min-h-80 bg-slate-900 text-slate-100 p-8 mb-16 rounded-xl">
+            <div className="text-center">
+                <h1 className="text-7xl/tight mb-2 font-extrabold">404</h1>
+                <p>A página que você está tentando acessar não existe nesse site</p>
+            </div>
+        </div>
+    )
+}
