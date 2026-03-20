2fa929bec7690015fc1bbb38b522c68b88f27c36
Thiago <thiago.fontanella@hotmail.com>
2026-03-19 22:56:38 -0300

    feat: use generateMetadata to create dynamic titles and descriptions

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,8 +1,18 @@
 import { findPostBySlug } from "@/lib/post/queries";
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
     params: Promise<{slug: string}>
+}
+
+export async function generateMetadata({ params }: PostSlugPageProps): Promise<Metadata> {
+    const { slug } = await params;
+    const post = await findPostBySlug(slug)
+
+    return {
+        title: post.title,
+        description: post.excerpt
+    }
 }
 
 export default async function PostSlugPage({ 
