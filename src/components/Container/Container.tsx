a1d731c58c848a918da2da8b7425678f15837801
Thiago <thiago.fontanella@hotmail.com>
2026-03-26 23:35:37 -0300

    fix: remove db from commit

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,15 +1,11 @@
 type ContainerProps = {
-    children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-export function Container({
-    children
-}: ContainerProps) {
-    return (
-        <div className="min-h-screen text-slate-900 bg-slate-100">
-            <div className="max-w-5xl mx-auto px-8">
-                {children}
-            </div>
-        </div>
-    )
+export function Container({ children }: ContainerProps) {
+  return (
+    <div className="min-h-screen text-slate-900 bg-slate-100">
+      <div className="max-w-5xl mx-auto px-8">{children}</div>
+    </div>
+  );
 }
--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -1,25 +1,25 @@
-'use client';
+"use client";
 
 type ErrorMessageProps = {
-    pageTitle: string,
-    contentTitle: string,
-    content: React.ReactNode
-}
+  pageTitle: string;
+  contentTitle: string;
+  content: React.ReactNode;
+};
 
 export function ErrorMessage({
-    pageTitle,
-    contentTitle,
-    content
+  pageTitle,
+  contentTitle,
+  content,
 }: ErrorMessageProps) {
-    return (
-        <>
-            <title>{pageTitle}</title>
-            <div className="flex justify-center items-center min-h-80 bg-slate-900 text-slate-100 p-8 mb-16 rounded-xl">
-                <div className="text-center">
-                    <h1 className="text-7xl/tight mb-2 font-extrabold">{contentTitle}</h1>
-                    <p>{content}</p>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <title>{pageTitle}</title>
+      <div className="flex justify-center items-center min-h-80 bg-slate-900 text-slate-100 p-8 mb-16 rounded-xl">
+        <div className="text-center">
+          <h1 className="text-7xl/tight mb-2 font-extrabold">{contentTitle}</h1>
+          <p>{content}</p>
+        </div>
+      </div>
+    </>
+  );
 }
