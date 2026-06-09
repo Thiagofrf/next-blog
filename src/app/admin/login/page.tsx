d005321daaa889a54902748fbb430040be8c9206
Thiago <thiago.fontanella@hotmail.com>
2026-06-08 23:14:25 -0300

    feat: start creating admin pages

---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,7 @@
+export default async function AdminLoginPage() {
+    return (
+        <div className="py-16 text-6xl">
+            AdminLoginPage
+        </div>
+    )
+}
---
+++ src/app/admin/post/page.tsx
@@ -0,0 +1,7 @@
+export default async function AdminPostPage() {
+    return (
+        <div className="py-16 text-6xl">
+            AdminPostPage
+        </div>
+    )
+}
